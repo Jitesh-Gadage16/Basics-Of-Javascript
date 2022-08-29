0f39487df33cdf42162b5a370b398fc4575f6ef3
Jitesh Gadage <gadagejitesh16@gmail.com>
2022-08-30 02:10:37 +0530

    answered till 12th questions

---
+++ variable.js
@@ -0,0 +1,15 @@
+
+
+var fullName = "Jitesh Gadage";
+var Age = 22.2;
+var isLoggedIn = true;
+var userName;
+var coursePurchased =null ;
+console.log( "fullName:", fullName + " " + "(type - " + typeof fullName + ")")
+console.log( "Age:", Age + " " + "(type - " + typeof Age + ")")
+console.log( "isLoggedIn:", isLoggedIn + " " + "(type - " + typeof isLoggedIn + ")")
+console.log( "userName:", userName + " " + "(type - " + typeof userName + ")")
+console.log( "coursePurchased:", coursePurchased + " " + "(type - " + typeof coursePurchased + ")")
+
+
+
